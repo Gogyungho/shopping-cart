f03e4799103704b3c8f1a533c106767048ffd4e0
Gogyungho <whohow10@gmail.com>
2022-11-11 03:29:19 +0900

    페이지 네이션 개발, 상품을 장바구니에 담고 빼는 버튼 작업

--- src/store/cart.ts
+++ src/store/cart.ts
@@ -21,10 +21,14 @@ export const cart = createSlice({
     INIT_CART_LISTS: (state, action: PayloadAction) => {
       state.cartLists = [];
     },
+
+    DELETE_CART_ITEM(state, action) {
+      state.cartLists = state.cartLists.filter((a) => a.item_no !== action.payload);
+    },
   },
 });
 
-export const { SET_CART_LISTS, INIT_CART_LISTS } = cart.actions;
+export const { SET_CART_LISTS, INIT_CART_LISTS, DELETE_CART_ITEM } = cart.actions;
 
 export const cartForm = (state: AppState): IProps => state.cart;
 export default cart.reducer;
